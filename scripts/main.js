f8dce2fb047a8b557d9f8bf8046bbf8c497bced0
Casey Baggz <casey@ovenbits.com>
2016-05-06 19:24:16 -0500

    add "under construction" note

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+(function() {
+
+  // window.onscroll = function() {
+  //   var featureImg = document.getElementById('feature'),
+  //       amount = document.body.scrollTop / 2;
+  //   featureImg.style.backgroundPositionY = amount + 'px';
+  // };
+
+})();
