633ac18a7097212a139d0891f6da1e410fea01a5
Casey Baggz <caseybaggz@gmail.com>
2016-09-19 13:50:14 -0500

    add bg images w/ aphrodite

--- caseybaggz/src/components/FooterLink.js
+++ caseybaggz/src/components/FooterLink.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import githubIco from '../images/ico/github.svg';
+import codepenIco from '../images/ico/codepen.svg';
+import twitterIco from '../images/ico/twitter.svg';
+import linkedinIco from '../images/ico/linkedin.svg';
 import '../stylesheets/FooterLink.css';
+
+
+const itemBg = StyleSheet.create({
+  github: {
+    backgroundImage: `url(${ githubIco })`
+  },
+  linkedin: {
+    backgroundImage: `url(${ linkedinIco })`
+  },
+  twitter: {
+    backgroundImage: `url(${ twitterIco })`
+  },
+  codepen: {
+    backgroundImage: `url(${ codepenIco })`
+  }
+});
+
 
 export default class FooterLink extends Component {
   render() {
     const itemStyle = {
       backgroundColor: '#546575',
-      backgroundImage: 'url("../images/ico/${ this.props.name }.svg")',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
       backgroundSize: '2em'
     };
 
     return (
-      <li className={ `footer-item ${ this.props.name }` } style={ itemStyle }>
+      <li className={ 'footer-item ' + css(itemBg[this.props.name]) } style={ itemStyle }>
         <a href={ this.props.link } className="link" target="_blank" />
       </li>
     );
--- caseybaggz/src/components/RecentItem.js
+++ caseybaggz/src/components/RecentItem.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import NavLink from './NavLink';
-import lays from '../images/backgrounds/lays.png';
-import thrive from '../images/backgrounds/thrive.png';
+import laysBg from '../images/backgrounds/lays.png';
+import thriveBg from '../images/backgrounds/thrive.png';
 import '../stylesheets/RecentItem.css';
+
+
+const itemBackground = StyleSheet.create({
+  lays: {
+    backgroundImage: `url(${ laysBg })`
+  },
+  thrive: {
+    backgroundImage: `url(${ thriveBg })`
+  }
+});
 
 
 export default class RecentItem extends Component {
   render() {
     const itemStyle = {
-      backgroundImage: `url(../images/backgrounds/${ this.props.name }.png)`,
       backgroundRepeat: 'no-repeat',
       backgroundPosition: 'center',
       backgroundColor: '#fff',
@@ -16,7 +26,7 @@ export default class RecentItem extends Component {
     };
 
     return (
-      <div className={ `c-recent-item ${ this.props.name }` } style={ itemStyle }>
+      <div className={ 'c-recent-item ' + css(itemBackground[this.props.name]) } style={ itemStyle }>
         <NavLink to={ `/works/${ this.props.name }` } className="link">
           <span className="info">
             <p className="type">{ this.props.stack }</p>
