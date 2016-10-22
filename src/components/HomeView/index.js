9aefe866fd568987254563a1e948c80753d4955c
Casey Baggz <caseybaggz@gmail.com>
2016-10-22 11:18:19 -0500

    Remove unused Components & Update Index

--- src/components/HomeView/index.js
+++ src/components/HomeView/index.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
-import RecentItem from '../RecentItem';
 import Footer from '../Footer';
 import MailBtn from '../MailBtn';
 import './HomeView.css';
 
 
 export default class HomeView extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      recentItems: [
-        { name: 'lays', stack: 'React, es6, & Rails' },
-        { name: 'thrive', stack: 'React & es6' },
-      ]
-    };
-  }
-
   render() {
     return (
-      <div className="home-view-container">
-        <div className="feature">
-          <p className="sub-headline">
-            React & Rails
-            <span className="br-mobile" />
-            Creative Developer
+      <div className="HomeView">
+        <div className="HomeView-feature">
+          <p className="HomeView-sub-headline">
+            FE-Engineer & Speaker
           </p>
         </div>
 
-        <div className="recent-work-container">
-          <p className="headline">recent work</p>
-          <div className="work-wrapper">
-            {
-              this.state.recentItems.map((item, index) => (
-                <RecentItem { ...item } key={ index } />
-              ))
-            }
-          </div>
+        <div className="HomeView-recent-container">
+          <p className="HomeView-recent-description">
+            Ey, yo! I'm Casey Baggz and I have the privilege to lead the
+            front-end team at
+            <a
+              className="HomeView-link"
+              href="https://www.whiplashmerch.com/"
+              target="_blank">
+              Whiplash
+            </a>.
+          </p>
+
+          <p className="HomeView-recent-description">
+            When I'm not cooking up a new React app or helping contribute to the
+            <a
+              className="HomeView-link cushion"
+              href="https://github.com/facebookincubator/create-react-app"
+              target="_blank">
+              create-react-app
+            </a>
+            cli, I'm usually continuing to build up our
+            <a
+              className="HomeView-link"
+              href="https://www.npmjs.com/package/whiplash-ui-library"
+              target="_blank">
+              whiplash-ui-library
+            </a> npm package.
+          </p>
+
+          <p className="HomeView-recent-description">
+            I speak at conferences if they are crazy enough to have me and I
+            love animals. What else do you want from me?
+          </p>
         </div>
 
-        <div className="cta-wrapper">
-          <div className="btn-wrapper">
+        <div className="HomeView-cta-wrapper">
+          <div className="HomeView-btn-wrapper">
             <MailBtn />
           </div>
         </div>
