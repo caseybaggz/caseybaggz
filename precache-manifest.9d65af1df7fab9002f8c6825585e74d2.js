720eff37691e86f843fbe0ace6053469db99dd08
Casey Baggz <caseybaggz@gmail.com>
2018-10-11 16:58:00 -0500

    Updates

---
+++ precache-manifest.9d65af1df7fab9002f8c6825585e74d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b606072c30e2e2fc31026373b2aeef0f",
+    "url": "/static/media/baggz.b606072c.svg"
+  },
+  {
+    "revision": "9aaa265d9fdf856c400c",
+    "url": "/static/css/main.ecc5b7eb.chunk.css"
+  },
+  {
+    "revision": "94b348dfb2a1b95e576f7534f2450ef7",
+    "url": "/static/media/s6.94b348df.svg"
+  },
+  {
+    "revision": "7d6eecd69644f30674a8",
+    "url": "/static/js/1.7d6eecd6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9aaa265d9fdf856c400c",
+    "url": "/static/js/main.9aaa265d.chunk.js"
+  },
+  {
+    "revision": "cba1328b35100fabfef3737c6db0c27a",
+    "url": "/static/media/github.cba1328b.svg"
+  },
+  {
+    "revision": "dd248a463e0246ac21ab434a45348f06",
+    "url": "/static/media/linkedin.dd248a46.svg"
+  },
+  {
+    "revision": "9b42f3a9e5a1b23f1bc3d423419c171f",
+    "url": "/static/media/twitter.9b42f3a9.svg"
+  },
+  {
+    "revision": "7d6eecd69644f30674a8",
+    "url": "/static/css/1.a1c2f2f6.chunk.css"
+  },
+  {
+    "revision": "4b07fe210be9bbefb3e1cfa07d24a3fe",
+    "url": "/index.html"
+  }
+];
