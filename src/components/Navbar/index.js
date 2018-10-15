8e00e4cc6f069475a91edec450308d6d87f7cf29
Casey Baggz <caseybaggz@gmail.com>
2018-10-15 07:33:32 -0500

    Update links in navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,11 +6,16 @@ import { Link } from 'gatsby';
 import NavStyles from './navbar.module.css';
 
 const links: Array<Object> = [
-  { id: 0, text: "Home", url: "/" },
-  { id: 1, text: "Color-blind", url: "/colorblind" },
-  { id: 2, text: "Speaking", url: "/speaking" },
-  { id: 3, text: "Posts", url: "/posts" },
-  { id: 4, text: "Contact", url: "/contact" }
+  { id: 0, text: "Home", url: "/", el: "link" },
+  { id: 1, text: "Color-blind", url: "/colorblind", el: "link" },
+  {
+    id: 2,
+    text: "Speaking",
+    url: "https://medium.com/@caseybaggz/post-squares-conf-2016-5ecac18387c",
+    el: ""
+  },
+  { id: 3, text: "Posts", url: "https://medium.com/@caseybaggz", el: "" },
+  { id: 4, text: "Contact", url: "mailto:caseybaggz@gmail.com", el: "" }
 ];
 
 export default class Navbar extends Component<{},{}> {
@@ -19,13 +24,23 @@ export default class Navbar extends Component<{},{}> {
   }
 
   getLinks = () => {
-    return links.map(obj => (
-      <li key={obj.id} className={NavStyles.li}>
-        <Link to={obj.url} className={NavStyles.link}>
-          {obj.text}
-        </Link>
-      </li>
-    ));
+    return links.map(obj => {
+      if (obj.el === 'link') {
+        return(
+          <li key={obj.id} className={NavStyles.li}>
+            <Link to={obj.url} className={NavStyles.link}>
+              {obj.text}
+            </Link>
+          </li>
+        );
+      }
+
+      return <li key={obj.id} className={NavStyles.li}>
+          <a href={obj.url} className={NavStyles.link}>
+            {obj.text}
+          </a>
+        </li>;
+    });
   }
 
   render() {
