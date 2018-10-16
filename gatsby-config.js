56fca5f98d76fc022ede15c21f439f26a60995b6
Casey Baggz <caseybaggz@gmail.com>
2018-10-16 07:49:37 -0500

    Add manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,18 @@ module.exports = {
       resolve: `gatsby-plugin-flow`
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#00BCD4",
+        theme_color: "#00BCD4",
+        display: "minimal-ui",
+        icon: "src/images/icon.png" // This path is relative to the root of the site.
+      }
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`
