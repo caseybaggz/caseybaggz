86fce10207c75a79b891a8bf3e59eb3be44412ed
Casey Baggz <caseybaggz@gmail.com>
2018-10-17 08:03:10 -0500

    Updates

--- sw.js
+++ sw.js
@@ -24,11 +24,61 @@ workbox.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-5e971d3fe08b81612204.js"
+  },
+  {
+    "url": "app-ccf960cc35037d9023eb.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-e3c47cb992e28d3ff0c9.js"
+  },
+  {
+    "url": "index.html",
+    "revision": "03b54ca169c09801c77e925f6ba1ec1b"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "18d8fcb635a6215cbba169894e1a8960"
+  },
+  {
+    "url": "1.9d9e8860578dd2781ebc.css"
+  },
+  {
+    "url": "google-fonts/s/opensans/v15/mem8YaGs126MiZpBA-UFVZ0b.woff2",
+    "revision": "cffb686d7d2f4682df8342bd4d276e09"
+  },
+  {
+    "url": "google-fonts/s/poppins/v5/pxiByp8kv8JHgFVrLEj6Z1xlFQ.woff2",
+    "revision": "d01ac68c4747e41a359cd94cecc29f9f"
+  },
+  {
+    "url": "1-6013d04477c1681f9c57.js"
+  },
+  {
+    "url": "component---src-pages-index-js-e3f4f1ed9e477a271555.js"
+  },
+  {
+    "url": "0-5e903cebf02c65a4cfe8.js"
+  },
+  {
+    "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "8700ff3ace150a85a94809a25d4255aa"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/caseybaggz/offline-plugin-app-shell-fallback/index.html", {
+workbox.routing.registerNavigationRoute("/offline-plugin-app-shell-fallback/index.html", {
   whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
   blacklist: [/\?(.+&)?no-cache=1$/],
 });
