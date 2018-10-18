9014239f7f35e2ae172dffd9421eb871a95499bf
Casey Baggz <caseybaggz@gmail.com>
2018-10-18 07:45:41 -0500

    Add google-analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,26 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-83053778-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Enables Google Optimize using your container Id
+        optimizeId: "UA-83053778-1",
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com"
+      }
+    },
+    {
       resolve: `gatsby-plugin-flow`
     },
     {
