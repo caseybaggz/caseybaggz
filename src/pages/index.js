acc6127974f627657e5b56b12621520be08270f0
Casey Baggz <caseybaggz@gmail.com>
2020-03-28 14:24:21 -0500

    Add initial Home Content Feature styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 // @flow
 
 import React from "react";
+import styled from 'styled-components';
 import HelmetMeta from '../components/layout/HelmetMeta';
 import Layout from '../components/layout/Layout';
+
+const Feature = styled.div`
+  padding-bottom: 65px;
+  padding-left: 31px;
+  padding-right: 31px;
+  padding-top: 53px;
+`;
 
 type Props = {};
 
@@ -10,7 +18,7 @@ function Home(props: Props): React$Node {
   return (
     <Layout>
       <HelmetMeta />
-      template
+      <Feature>template</Feature>
     </Layout>
   );
 }
