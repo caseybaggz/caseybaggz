bc18356fc96429a702efa706309854e75644c640
Casey Baggz <caseybaggz@gmail.com>
2020-03-29 12:21:37 -0500

    Touch up styles and fonts to be lager

--- src/components/actions/Button.js
+++ src/components/actions/Button.js
@@ -9,12 +9,11 @@ const Button = styled(Link)(
   border-radius: 30px;
   color: ${props.theme.gray1};
   display: inline-block;
-  font-size: 14px;
   font-weight: 600;
-  padding-bottom: 10px;
+  padding-bottom: 10.5px;
   padding-left: 21px;
   padding-right: 21px;
-  padding-top: 10px;
+  padding-top: 10.5px;
   transition: background-color 250ms ease-out;
   text-align: center;
   width: ${props.width || 'initial'};
--- src/components/typography/Headline.js
+++ src/components/typography/Headline.js
@@ -3,13 +3,15 @@
 import styled from 'styled-components';
 import media from '../../utils/media';
 
+const headlineFontSize: string = '38px';
+
 const Headline = styled.h3(
   props => `
   color: ${props.theme[props.color] || props.theme.gray3};
-  font-size: 32px;
+  font-size: ${headlineFontSize};
 
   ${media.large} {
-    font-size: ${props.responsive ? '64px' : '32px'};
+    font-size: ${props.responsive ? '76px' : headlineFontSize};
   }
 `
 );
