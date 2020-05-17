bcada65eb342d1a47f2d93d7b2879b32ea5f8a89
Casey Baggz <caseybaggz@gmail.com>
2020-05-17 12:26:22 -0500

    Add initial test file for jest confirmation

--- src/components/__tests__/emoji.test.js
+++ src/components/__tests__/emoji.test.js
@@ -1,5 +1,11 @@
-
+import React from 'react';
+import { render } from '@testing-library/react';
+// import Emoji from '../Emoji';
 
 describe('Emoji', () => {
-  it('renders without crashing', () => {});
+  test('renders learn react link', () => {
+    // const { getByText } = render(<Emoji symbol="👏" />);
+    // const linkElement = getByText(/👏/i);
+    // expect(linkElement).toBeInTheDocument();
+  });
 });
